29f9efc34cfe4833c30ce7d3260ae6a20a7df5ee
northonnstaff <northonnstaff@users.noreply.github.com>
2026-05-18 23:24:46 -0300

    Initial FastAPI API

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {
+        "mensagem": "Hello World da API Python",
+        "status": "online",
+        "origem": "FastAPI"
+    }
+
+
+@app.get("/hello")
+def hello(nome: str = "Oracle APEX"):
+    return {
+        "mensagem": f"Olá, {nome}!",
+        "data_hora": datetime.now().isoformat()
+    }
+
+
+class MensagemRequest(BaseModel):
+    nome: str
+
+
+@app.post("/hello")
+def hello_post(request: MensagemRequest):
+    return {
+        "mensagem": f"Olá, {request.nome}! Recebido via POST.",
+        "status": "sucesso"
+    }
